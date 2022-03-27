d1731faf79c92b37af10244f86482b94cbab9fcf
Patrick Sparks <pto1026@gmail.com>
2022-03-26 22:46:01 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+  <h2>My Friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: 'manuel',
+          name: 'Manuel Lorenz',
+          phone: '0123 4567 890',
+          email: 'manuel@localhost.com'
+        },
+        {
+          id: 'julie',
+          name: 'Julie Jones',
+          phone: '0987 6543 211',
+          email: 'julie@localhost.com'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
